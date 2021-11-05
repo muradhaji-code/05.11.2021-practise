6b069b2db026fac5019bd3d763491e8995584be8
Hajiyev <tu201806256@aztu.local>
2021-11-05 13:47:31 +0400

    init

---
+++ 05.11.2021-practise/Milk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._11._2021_practise
+{
+    class Milk : Product
+    {
+        public double Volume { get; set; }
+        public double FatRate { get; set; }
+        public Milk(string Name, double Price, int Count, double Volume, double FatRate)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Count = Count;
+            this.TotalIncome = 0;
+            this.Volume = Volume;
+            this.FatRate = FatRate;
+        }
+        public bool isValid()
+        {
+            if (this.Name == "") return false;
+            return true;
+        }
+    }
+}
---
+++ 05.11.2021-practise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05._11._2021_practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
